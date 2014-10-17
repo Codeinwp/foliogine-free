a80750089ab71f1a85afb99cb416a7c0cf69b95e
Marius Cristea <cristea.marius.alexandru@gmail.com>
2014-10-17 15:15:18 -0500

    Sync with developemnt

--- tags/1.2.0/404.php
+++
@@ -1,19 +0,0 @@
-<?php
-/**
- * The template for displaying 404 pages (Not Found).
- *
- * @package foliogine
- */
-?>
-<?php get_header(); ?>
-<section class="title-page-area">				
-	<div class="container">					
-		<h1><?php _e( 'Oops! That page can&rsquo;t be found.', 'cwp' ); ?></h1>				
-	</div><!-- .container -->			
-</section><!-- .title-page-area -->	
-<section>
-    <div class="container">
-	   <p><?php _e( 'It looks like nothing was found at this location', 'cwp' ); ?></p>					
-    </div>
-</section>    
-<?php get_footer(); ?>
--- tags/1.2.0/inc/brochure.php
+++
@@ -1,9 +0,0 @@
-<?php 	$download_url = cwp('download_url');	$download_text = cwp('download_text');	$download_title = cwp('download_title');	$download_url = cwp('download_url');		if(isset($download_url) && $download_url != ''):?>	
-		<section class="download">
-			<div class="container">
-                <?php 					if(isset($download_text) && $download_text != ''):
-						echo '<a class="text">'.$download_text.'</a>';					endif;
-					if(isset($download_title) && $download_title != '' && isset($download_url) && $download_url != ''):						echo '<a href="'.$download_url.'" class="button" title="Download">'.$download_title.'</a>';					endif;					?>        
-			</div><!-- .container -->
-		</section><!-- .download -->
-<?php endif; ?>
